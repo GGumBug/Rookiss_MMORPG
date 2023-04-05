5fde0f4f5509b81c33503d48bd298eabcf4070b3
GGumBug <alsrl7538@gmail.com>
2023-04-05 10:55:32 +0900

    Pooling Test 삭제 및 Coroutine 주석 추가

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    // Coroutine
+    // 1. 함수의 상태를 저장 /복원 가능!
+    // -> 엄청 오래 걸리는 작업을 잠시 끊거나
+    // -> 원하는 타이밍에 함수를 잠시 Stop/복원 하는 경우
+    // 2. return -> 우리가 원하는 타입으로 가능 (class도 가능)
+
     protected override void Init()
     {
         base.Init();
@@ -11,9 +17,6 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        for (int i = 0; i < 5; i++)
-            Managers.Resource.Instantiate("UnityChan");
     }
 
     public override void Clear()
